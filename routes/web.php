0ee7dd4f674ad8b69263584983790ce3ffe95e35
@muhammad-ari-kuncoro <@muhammad-ari-kuncoro>
2024-07-14 14:33:48 +0700

    layouting web-v2

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('layouts.mainLoginPage');
+    return view('Auth.loginPage');
 });
 
 // Route::get('/', function () {
