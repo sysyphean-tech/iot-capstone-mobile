59233e176c17acaccca8ec39cf60359ca2bec080
@muhammad-ari-kuncoro <@muhammad-ari-kuncoro>
2024-07-14 22:00:51 +0700

    layouting web-v2-controller-update

---
+++ resources/views/component/footerHome.blade.php
@@ -0,0 +1,18 @@
+
+</div>
+</div>
+<script src="assets/static/js/components/dark.js"></script>
+<script src="assets/extensions/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+
+
+<script src="assets/compiled/js/app.js"></script>
+
+
+
+<!-- Need: Apexcharts -->
+<script src="assets/extensions/apexcharts/apexcharts.min.js"></script>
+<script src="assets/static/js/pages/dashboard.js"></script>
+
+</body>
+
+</html>
